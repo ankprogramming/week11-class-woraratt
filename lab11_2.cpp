0d605bd5b4daa9bbc4fed2ee569c0fe20be4b07e
Unknown <mymintzata@hotmail.co.th>
2018-02-06 11:46:51 +0700

    ข้อ 2 เสร็จ

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,5 +6,21 @@ using namespace std;
 int main() {
     string str = "There are two needles in this haystack with needles.";
     cout << str << endl;
+    cout << "Length : " << str.length() << endl;
+
+    str += " I Want to eat needle. ";
+    cout << str << endl;
+    cout << "Length : " << str.length() << endl;
+
+    str.erase(str.length()/2, str.length()/2);
+    cout << str << endl;
+    cout << "Length : " << str.length() << endl;
+
+    if (str.find("needle") < str.length()) {
+       cout << "Found 'needle'" << endl;
+    } else {
+        cout << "Not Found 'needle'" << endl;
+    }
+
     return 0;
 }
